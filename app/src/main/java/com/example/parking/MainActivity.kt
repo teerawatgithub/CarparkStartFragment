d6ec37e228d783ab91a0eb25bc9e167b370f141b
teerawatgithub <59160069@go.buu.ac.th>
2019-08-22 15:33:12 +0700

    test data binding

--- app/src/main/java/com/example/parking/MainActivity.kt
+++ app/src/main/java/com/example/parking/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.parking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.parking.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
+    private var parking: Parking = Parking("Teerawat", "test", "test", "0214563654")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.parking = parking
+
     }
 }
---
+++ app/src/main/java/com/example/parking/Parking.kt
@@ -0,0 +1,3 @@
+package com.example.parking
+
+data class Parking(var name: String, var licensePlate: String, var carBrand: String, var telNumber: String)
